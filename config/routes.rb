a14205b53bbb5aa91064467a5ce7556456819285
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-11-30 16:20:01 +0300

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :signups, only: [:index]
+  resources :activities, only: [:index]
+  resources :campers, only: [:index, :show]
+
 end
